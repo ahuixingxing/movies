8109a3dc881c6b714bc9fee3990f404cf35052cc
Ahui <ahuixingxing@163.com>
2019-04-15 20:02:32 +0800

    第15章完成

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 # _*_coding:utf-8_*_
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+import os
+
 
 app=Flask(__name__)     #实例化Flask类得到app对象
 app.debug=True          #开启调试模式
@@ -8,6 +10,8 @@ app.debug=True          #开启调试模式
 app.config["SQLALCHEMY_DATABASE_URI"]="mysql+mysqlconnector://root:root@88.10.0.15:3306/movie"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=True
 app.config["SECRET_KEY"]='b1b7ed6af47d4031acbdeb420658ba84'
+#定义文件上传保存的路径,在__init__.py文件所在的目录创建media文件夹,用于保存上传的文件
+app.config['UP_DIR']=os.path.join(os.path.abspath(os.path.dirname(__file__)),'static/media/')
 
 #定义db对象,实例化SQLAlchemy,传入app对象
 db=SQLAlchemy(app)
